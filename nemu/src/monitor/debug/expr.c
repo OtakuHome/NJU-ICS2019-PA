267402e5c7a45ee87b4cc8d2f61ce4ee5cf960e7
tracer-ics2019 <tracer@njuics.org>
2020-07-25 11:52:49 +0800

    >  compile 1034180228 ccl Linux debian 4.19.0-9-amd64 #1 SMP Debian 4.19.118-2+deb10u1 (2020-06-07) x86_64 GNU/Linux  11:52:49 up  3:49,  1 user,  load average: 0.02, 0.01, 0.00 df847df7801fbec79650e91cfa5ddacd94031ca7

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -5,34 +5,40 @@
  */
 #include <sys/types.h>
 #include <regex.h>
+#include <stdlib.h>
+
+uint32_t isa_reg_str2val(const char *s, bool *success);
 
 enum {
-  TK_NOTYPE = 256, TK_EQ, TK_NUM
-
+  TK_NOTYPE = 256, TK_EQ, TK_NUM, TK_PLUS, TK_SUB, TK_MUL, TK_DIV,
+  TK_LBR, TK_RBR, TK_HEX, TK_REG, TK_AND, TK_OR, TK_DEREF
   /* TODO: Add more token types */
-
 };
 
 static struct rule {
   char *regex;
   int token_type;
 } rules[] = {
-
   /* TODO: Add more rules.
    * Pay attention to the precedence level of different rules.
    */
 
-  /* TODO: PA1.5 */
+  /* PA1.5 */
+  // TODO: rename all the token_type to enum.
 
-  {" +", TK_NOTYPE},    // spaces
-  {"\\+", '+'},         // plus
-  {"==", TK_EQ},        // equal
-  {"-", '-'},			// substract
-  {"\\*", '*'},			// multiply
-  {"/", '/'},			// divide
-  {"[0-9]+", TK_NUM},		// number
-  {"\\(", '('},			// left bracket
-  {"\\)", ')'}			// right bracket
+  {" +", TK_NOTYPE},				// spaces
+  {"\\+", TK_PLUS},					// plus
+  {"==", TK_EQ},					// equal
+  {"-", TK_SUB},					// substract
+  {"\\*", TK_MUL},					// multiply or derefrence
+  {"/", TK_DIV},					// divide
+  {"[0-9]+", TK_NUM},				// number(dec)
+  {"\\(", TK_LBR},					// left bracket
+  {"\\)", TK_RBR},					// right bracket
+  {"0[Xx][0-9a-fA-F]+", TK_HEX},	// hex
+  {"\\$[a-zA-Z]+", TK_REG},			// register
+  {"&&", TK_AND},					// AND
+  {"||", TK_OR}						// OR
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
@@ -62,7 +68,7 @@ typedef struct token {
 } Token;
 
 /* TODO: Change the size of the Token array for test */
-#define NR_TOKEN 600
+#define NR_TOKEN 32
 static Token tokens[NR_TOKEN] __attribute__((used)) = {};
 static int nr_token __attribute__((used))  = 0;
 
@@ -103,6 +109,8 @@ static bool make_token(char *e) {
 			case TK_NOTYPE:
 				break;
 			case TK_NUM:
+			case TK_HEX:
+			case TK_REG:
 				strncpy(tokens[nr_token].str, substr_start, substr_len);
 				tokens[nr_token].str[substr_len] = '\0';
 			default: 
@@ -134,56 +142,64 @@ static bool make_token(char *e) {
 int check_parentheses(int p, int q)
 {
 	bool LR = false;
-	if(tokens[p].type == '(' && tokens[q].type ==')') LR = true;
-	char stack[36];
+	if(tokens[p].type == TK_LBR && tokens[q].type ==TK_RBR) LR = true;
 	int top = 0, i;
 	for(i = p; i <= q; ++ i){
-		if(tokens[i].type == '(') stack[++ top] = '(';
-		else if(tokens[i].type == ')') {
-			if(top > 0 && stack[top] == '(') --top;
-			else return -1;	
-		}
+		if(tokens[i].type == TK_LBR) ++ top;
+		else if(tokens[i].type == TK_RBR) -- top;
+		if(top < 0) return -1;
 	}
+	if(top != 0) return -1;
 	if(!LR) return 0;
 
-	/* Judge such case like: (4 + 5) - (6 - 1)  */
-	top = 0;
+	/* Beware of such case like: (4 + 5) - (6 - 1)  */
+	
 	for(i = p + 1; i <= q - 1; ++ i){
-		if(tokens[i].type == '(') stack[++ top] = '(';
-		else if(tokens[i].type == ')') {
-			if(top > 0 && stack[top] == '(') --top;
-			else return 0;	
-		}
-		
+		if(tokens[i].type == TK_LBR) ++ top;
+		else if(tokens[i].type == TK_RBR) -- top;
+		if(top < 0) return 0;
 	}
-
-	return 1;
+	return 1;	// top must be zero
 }
 
-bool isOp(int ch){
-	return ch == '+' || ch == '-' || ch == '*' || ch == '/';
+bool is_op(int ch){
+	return ch == TK_PLUS || ch == TK_SUB || ch == TK_MUL || ch == TK_DIV
+			|| ch == TK_AND || ch == TK_OR || ch == TK_EQ || ch == TK_DEREF;
 }
 /* return the priority of the oprator*/
 int op_priority(int op)
 {
 	int pri;
 	switch(op){
-		case '+':
-		case '-':
+		case TK_OR:
+			pri = 0;
+			break;
+		case TK_AND:
 			pri = 1;
 			break;
-		case '*':
-		case '/':
+		case TK_EQ:
 			pri = 2;
+			break;
+		case TK_PLUS:
+		case TK_SUB :
+			pri = 3;
+			break;
+		case TK_MUL:
+		case TK_DIV:
+			pri = 4;
+			break;
+		case TK_DEREF:
+			pri = 5;
 			break;
 		default:
 			pri = 10;
 	}
 	return pri;
 }
-int compare(int op1, int op2){
-	int p1 = op_priority(op1);
-	int p2 = op_priority(op2);
+
+int compare(int i, int j){
+	int p1 = op_priority(tokens[i].type);
+	int p2 = op_priority(tokens[j].type);
 	return p1 < p2 ?  -1 : (p1 == p2 ? 0 : 1);
 }
 
@@ -193,12 +209,12 @@ int get_main_op(int p, int q)
 	int inBracket = 0, i, pos = -1;
 	for(i = p; i <= q; ++ i) {
 		int type = tokens[i].type;
-		if( !inBracket && isOp(type)){
+		if( !inBracket && is_op(type)){
 			if(pos == -1) pos = i;
-			else if(compare(type, tokens[pos].type) <= 0 ) pos = i; 
+			else if(compare(i, pos) <= 0 ) pos = i; 
 		}
-		else if(type == '(' ) inBracket ++ ;
-		else if(type == ')' ) inBracket -- ;
+		else if(type == TK_LBR ) inBracket ++ ;
+		else if(type == TK_RBR ) inBracket -- ;
 	}
 	return pos;
 }
@@ -215,10 +231,16 @@ uint32_t eval(int p, int q, bool *success)
 	if(p > q) {
 		return myexit(p, q, success);
 	}else if(p == q){
-		if(tokens[p].type == TK_NUM) {
-			uint32_t val = 0;
-			sscanf(tokens[p].str, "%u", &val);
-			return val;
+		uint32_t val = 0;
+		int type = tokens[p].type;
+		if(type == TK_NUM || type == TK_HEX) {
+			return strtoul(tokens[p].str, NULL, 0);
+		}
+		else if(type == TK_REG) {
+			val = isa_reg_str2val(tokens[p].str + 1, success);
+			if(success) return val;
+			printf("Unknown register: %s\n", tokens[p].str);
+			return 0; 
 		}
 		return myexit(p, q, success);
 	}
@@ -237,45 +259,95 @@ uint32_t eval(int p, int q, bool *success)
 		return myexit(p, q, success);
 	}
 		
-	uint32_t val1, val2, val;
-	val1 = eval(p, pos - 1, success);
-	if(*success == 0) return 0;
+	uint32_t val1 = 0, val2 = 0, val = 0;
+	if(tokens[pos].type != TK_DEREF)
+		val1 = eval(p, pos - 1, success);
+
+	if(*success == false) return 0;
 	val2 = eval(pos + 1, q, success);
-	if(*success == 0) return 0;
+	if(*success == false) return 0;
 
 	switch(tokens[pos].type){
-		case '+':
+		case TK_PLUS:
 			val = val1 + val2;
 			break;
-		case '-':
+		case TK_SUB:
 			val = val1 - val2;
 			break;
-		case '*':
+		case TK_MUL:
 			val = val1 * val2;
 			break;
-		case '/':
+		case TK_DIV:
 			if(val2 == 0) {
 				printf("Divide 0 error at [%d, %d]", p, q);
 				return *success = false;
 			}
 			val = val1 / val2;
 			break;
+		case TK_AND:
+			val = val1 && val2;
+			break;
+		case TK_OR:
+			val = val1 || val2;
+			break;
+		case TK_EQ:
+			val = val1 == val2;
+			break;
+		case TK_DEREF:
+			
 		default:
-			return myexit(p, q, success);
+			printf("Unknown token type: %d\n", tokens[pos].type);
+			return *success = false;
 	}
 
 	return val;
 }
 
+/* erase the index of the tokens array between [p, p + cnt - 1] */
+void erase(int p, int cnt){
+	int i;
+	for(i = p; i + cnt < nr_token; ++ i){
+		tokens[i] = tokens[i + cnt];
+	}
+	nr_token -= cnt;
+}
+
+/* PA1.5 1.6
+ * Date: 2020/7/24
+ */ 
 uint32_t expr(char *e, bool *success) {
   if (!make_token(e)) {
     *success = false;
     return 0;
   }
-
   /* TODO: Insert codes to evaluate the expression. */
 
-  uint32_t ret = eval(0, nr_token - 1, success);
+  /* Merge +- ++ -- -+ */  
+  int i, type;
+  for(i = 0; i < nr_token; ++ i){	
+	type = tokens[i].type; 
+	if(type == TK_PLUS || type == TK_SUB)
+	{
+		int j = i;
+		int flag = 1;
+		while(j < nr_token && (type == TK_PLUS || type == TK_SUB)){
+			flag *= (type == TK_PLUS ? 1 : -1);
+			type = tokens[++ j].type;
+		}
+		if(j - i > 1){
+			tokens[i].type = (flag == 1? TK_PLUS : TK_SUB) ;
+			erase(i + 1, j - i - 1);
+		}
+	}
+  } 
+   
+  for(i = 0; i < nr_token; ++ i){
+	if(tokens[i].type == TK_MUL && (i == 0 || is_op(tokens[i - 1].type))){
+		if(i > 0 && tokens[i - 1].type != TK_DEREF)	// Maybe this case: **
+	    tokens[i].type = TK_DEREF;
+	}
+  }
 
+  uint32_t ret = eval(0, nr_token - 1, success);
   return ret;
 }
