a72ccfe5a23a741583e5fcb90d3568cda5088fc3
gitccl <chenchuanle6@gmail.com>
2020-08-01 23:05:56 +0800

    >  compile 1034180228 ccl Linux debian 4.19.0-9-amd64 #1 SMP Debian 4.19.118-2+deb10u1 (2020-06-07) x86_64 GNU/Linux  23:05:56 up 12:16,  1 user,  load average: 0.25, 0.14, 0.09 7b9337b8b2331066cf430da4aa72c96c133978f

--- nemu/src/isa/x86/exec/all-instr.h
+++ nemu/src/isa/x86/exec/all-instr.h
@@ -24,6 +24,7 @@ make_EHelper(call);
 make_EHelper(ret);
 make_EHelper(jcc);
 make_EHelper(jmp);
+make_EHelper(call_rm);
 
 // arith.c
 make_EHelper(sub);
--- nemu/src/isa/x86/exec/control.c
+++ nemu/src/isa/x86/exec/control.c
@@ -45,7 +45,9 @@ make_EHelper(ret_imm) {
 }
 
 make_EHelper(call_rm) {
-  TODO();
-
+  decinfo.jmp_pc = id_dest->val;
+  rtl_push(&decinfo.seq_pc);
+  rtl_j(decinfo.jmp_pc);
+  
   print_asm("call *%s", id_dest->str);
 }
