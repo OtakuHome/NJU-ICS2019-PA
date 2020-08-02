f08bea5544f83fa67f346190a4191e26c23fe1e5
gitccl <chenchuanle6@gmail.com>
2020-08-02 13:30:16 +0800

    >  run 1034180228 ccl Linux debian 4.19.0-9-amd64 #1 SMP Debian 4.19.118-2+deb10u1 (2020-06-07) x86_64 GNU/Linux  13:30:16 up  4:56,  1 user,  load average: 0.10, 0.17, 0.09 a40900335f2d1840bb397bd3b5a848f6e6a444a3

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -51,8 +51,9 @@ int strcmp(const char* s1, const char* s2) {
 	//assert(*s2 == 'b');
 	
 	int ret = *s1 - *s2;
+	assert(1);
 	
-	assert(ret == -1);
+	//assert(ret == -1);
 	//ret = 0;
 	//assert(0);
 	if(ret > 0) return 1;
