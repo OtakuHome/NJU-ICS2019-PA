dfc5b7014a7855bd593dbbce3496f118ec030173
gitccl <chenchuanle6@gmail.com>
2020-08-02 17:09:37 +0800

    >  run 1034180228 ccl Linux debian 4.19.0-9-amd64 #1 SMP Debian 4.19.118-2+deb10u1 (2020-06-07) x86_64 GNU/Linux  17:09:37 up  8:35,  1 user,  load average: 0.10, 0.16, 0.08 c6d9c3f3569ab896e433e5c11fef9c6322ad72e8

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -46,8 +46,12 @@ int strcmp(const char* s1, const char* s2) {
 	}
 	
 	if(*s1 == *s2) return 0;
+	int ret = *(unsigned char *)s1 - *(unsigned char *)s2;
 	
-	return  *(unsigned char *)s1 - *(unsigned char *)s2;
+	if(ret > 0) ret = 1;
+	else if(ret < 0) ret = -1;
+	
+	return  ret;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
