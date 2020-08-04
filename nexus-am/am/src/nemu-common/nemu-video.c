6a0354a89bfa6cadfa1878efec1e3c06d4221b20
Chen Chuanle <chenchuanle6@gmail.com>
2020-08-04 20:43:22 +0800

    complete video

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -22,22 +22,20 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
 	switch (reg) {
 		case _DEVREG_VIDEO_FBCTL: {
 		  	_DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
-			if (ctl->sync) 
-			{
-				int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+		  	int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
 				
-				/* fb: the whole data that will be painted on the screen, 
-				 * but we only need to fill part of data (that is from (x, y) to (x + w, y + h)) by copy from pixels
-				 */
-				uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR; 
-				
-				/* pixels: the data that will be painted. we show copy the data in pixels to fb. */
-				uint32_t* pixels = ctl->pixels;
-				int copy_size = sizeof(uint32_t) * ( W - x < w ? W-x : w);
-				for(int i = 0; i < h && y + i < H; ++ i) {
-					memcpy(&fb[(i + y) * W + x], pixels , copy_size);
-					pixels += w;
-				}
+			/* fb: the whole data that will be painted on the screen, 
+			 * but we only need to fill part of data (that is from (x, y) to (x + w, y + h)) by copy from pixels
+			 */
+			uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR; 
+			
+			/* pixels: the data that will be painted. we show copy the data in pixels to fb. */
+			uint32_t* pixels = ctl->pixels;
+			int copy_size = sizeof(uint32_t) * ( W - x < w ? W-x : w);
+			for(int i = 0; i < h && y + i < H; ++ i) {
+				memcpy(&fb[(i + y) * W + x], pixels + i * w, copy_size);
+			}
+			if (ctl->sync) {
 				outl(SYNC_ADDR, 0);
 			}
 			return size;
