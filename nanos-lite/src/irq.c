4ed765291da765dd6c4dfd42914f0df39df0ddc3
Chen Chuanle <chenchuanle6@gmail.com>
2020-08-17 21:42:44 +0800

    successfully run hello_fun

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,12 +1,12 @@
 #include "common.h"
 
 _Context* do_syscall(_Context *c);
+_Context* schedule(_Context *prev);
 
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
   	case _EVENT_YIELD:
-  		printf("yield called success...\n");
-  		break;
+  		return schedule(c);
   	case _EVENT_SYSCALL:
   		return do_syscall(c);
     default: panic("Unhandled event ID = %d", e.event);
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -7,6 +7,7 @@ static PCB pcb_boot = {};
 PCB *current = NULL;
 
 void naive_uload(PCB *pcb, const char *filename);
+void context_kload(PCB *pcb, void *entry);
 
 void switch_boot_pcb() {
   current = &pcb_boot;
@@ -22,15 +23,19 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
+  context_kload(&pcb[0], (void *)hello_fun);
   switch_boot_pcb();
 
   Log("Initializing processes...");
 
   // load program here
-  naive_uload(NULL, "/bin/init");
+  // naive_uload(NULL, "/bin/init");
   
 }
 
 _Context* schedule(_Context *prev) {
-  return NULL;
+  current->cp = prev;
+  current = &pcb[0];
+  
+  return current->cp;
 }
